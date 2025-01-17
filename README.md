6222c70bfa0af23d9dd4d5914c2b65cb30a1edf5
Henrik Oskarsson <heos2200@student.miun.se>
2025-01-17 19:33:09 +0100

    Big rework, changes list explained in readme.md

--- README.md
+++ README.md
@@ -2,23 +2,25 @@
 
 This Sudoku solver lets the user enter a sudoku puzzle into a GUI that can then be solved by the press of a button.
 The program utilizes a brute force approach where every number is tried until a solution is reached. It was developed back in 2013 to 
-try out some recursive coding. 
+try out some recursive coding. In more modern terms, the program could be called a recursive backtracking constraint
+search.
 
 What makes this project interesting is what is provided in terms of functionality in comparison to the amount of code. It was written before
-any best practices was learned, and thus illuminates what can be achieved by a barebone implementation. Some highlights are:
+any best practices were learned, and illustrates what can be achieved by a barebone implementation. Some highlights are:
 
 - Works on any puzzle that has a solution
-- GUI representation with key-listeners for input
-- Mouse click listener using click location arithmetic to trim away much logic
-- GUI rendering using overlapping rectangles. Board effectively painted using 18 rectangles.
-- Everything fitted into < 200 lines of code
+- Easily portable to any language or frameworks that exposes a canvas to draw (basically any language)
+- Compatible all the way back to at least java 8
+- GUI rendered using only Graphics2D functions .drawString, .drawRect, .fillrect
+- Mouse click listener using click location arithmetic
+- Everything fitted into 100 lines of code
 
 How it solves using method _Boolean solve()_:
 1. Finds first empty field.
 2. Try every number in order iteratively (loop)
    3. When valid move found:
-      1. If board filled, return true
-      2. Else Recursively call, and return its value (1)
+      1. If board complete, return true
+      2. Else Recursively call solve(), and return its value (1)
    3. If no valid move:
       4. Clear last move and return false (i.e. backtrack)
 
@@ -28,5 +30,19 @@ Whenever it encounters a situation where no valid moves are present, it will bac
 tries before finding a solution, and might for some very specific low-information and single solution puzzles lead to very long solve times. However,
 the usual case is <1ms, and solving an empty board, it turns out, is actually trivial.
 
-### Future work
-- Condense program even more
+### Changes 2025-01-17
+- Pruned down redundant print logic
+- Clarified logic by using COLUMNS variable in many places (it still equals ROWS)
+- Refactored into a single class, saving some constructor logic, and making it a bit more tidy
+- Now checks for invalid input, hindering the user from triggering an exhaustive search taking forever
+- Removed unused variables
+- Condensed key listener logic
+- Condensed if statements with simple actions, moving action to same line
+- Condensed simple double for-loops by omitting brackets
+- Flattened some double for-loops
+- Reusing findEmptyField to check for completed condition
+- Arguably went to far in condensing logic...
+
+### Future ideas
+- Try out a one-dimensional array for board storage (can in theory shorten some logic i.e. double looping)
+- Consider maintaining a pointer for current position (avoids expensive lookups with findEmptyField())
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -2,47 +2,25 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Sudoku extends JFrame {
-	public static void main(String[] args) {
-		new Sudoku("Sudoku");
-	}
-
-	public Sudoku(String name) {
-		super(name);
-		add(new GamePanel());
-		setVisible(true);
-		pack();
-	}
-}
-
-class GamePanel extends JPanel {
-	static final int ROWS = 9;
-	static final int COLUMNS = ROWS;
-	static final int XSIZE = 495;
-	static final int YSIZE = XSIZE;
-	static final int ROW_WIDTH = XSIZE / ROWS;
-	static final int HIGHT_PADDING = 5;
-	static final int BORDER_THICKNESS = 1;
-	static final int SOLVE = KeyEvent.VK_S;
-	static final int CLEAR = KeyEvent.VK_C;
-
-	Color rowColor = new Color(100, 50, 50, 50);
-	Color columnColor = rowColor;// new Color(50, 50, 100, 40);
-	Color defaultColor = new Color(230, 255, 255);
-
+public class Sudoku extends JPanel {
+	final int ROWS = 9, COLUMNS = 9, SIZE = 495, ROW_WIDTH = SIZE / ROWS, SOLVE = KeyEvent.VK_S, CLEAR = KeyEvent.VK_C;
 	int[][] fields = new int[ROWS][COLUMNS];
 	Point selection = new Point();
 
-	public GamePanel() {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Sudoku Solver");
+		frame.add(new Sudoku());
+		frame.setVisible(true);
+		frame.pack();
+	}
+
+	public Sudoku() {
 		clearField();
-		setPreferredSize(new Dimension(XSIZE + BORDER_THICKNESS, YSIZE
-				+ BORDER_THICKNESS +ROW_WIDTH/2 + HIGHT_PADDING));
+		setPreferredSize(new Dimension(SIZE + 1, SIZE + ROW_WIDTH));
 		addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
-				Point p = e.getPoint();
-				p = coordToField(p);
-				selection = p;
-				requestFocusInWindow();
+				selection =  new Point(e.getPoint().y * COLUMNS / SIZE, e.getPoint().x * COLUMNS / SIZE);
+				requestFocusInWindow(); // Needed to activate key-listener
 				repaint();
 			}
 		});
@@ -55,138 +33,68 @@ class GamePanel extends JPanel {
 				}
 				if (code >= KeyEvent.VK_0 && code <= KeyEvent.VK_9) {
 					code -= KeyEvent.VK_0;
-					fields[selection.x][selection.y] = code;
-					repaint();
+					if(checkValidMove(selection, code)) fields[selection.x][selection.y] = code;
 				}
-				if (code == SOLVE) {
-					solve();
-					repaint();
-				}
-				if (code == CLEAR) {
-					clearField();
-					repaint();
-				}
+				if (code == SOLVE && findEmptyField() != null) solve();
+				if (code == CLEAR) clearField();
+				repaint();
 			}
 		});
 	}
 	
 	void clearField(){
-		for (int i = 0; i < ROWS; i++) {
-			for (int j = 0; j < ROWS; j++) {
-				fields[i][j] = 0;
-			}
-		}
+		for (int i = 0; i < COLUMNS * ROWS; i++) fields[i%COLUMNS][i/ROWS] = 0;
 	}
 
 	Boolean solve() {
-		Point emptyField = findEmptyField();
+		Point field = findEmptyField();
 		for (int i = 1; i <= ROWS; i++) {
-			fields[emptyField.x][emptyField.y] = i;
-			Boolean isValidMove = checkValidMove(emptyField, i);
-			if(isValidMove){
-				if(isComplete())    return true;
-				else if(solve()) 	return true;
-			}
+			fields[field.x][field.y] = i;
+			if(checkValidMove(field, i) && ((findEmptyField() == null) || solve())) return true;
 		}
-		fields[emptyField.x][emptyField.y] = 0;
+		fields[field.x][field.y] = 0; // restore value if backtracking
 		return false;
 	}
 
-	Boolean isComplete() {
-		for (int i = 0; i < ROWS; i++) {
-			for (int j = 0; j < ROWS; j++) {
-				if (fields[i][j] == 0)
-					return false;
-			}
-		}
-		return true;
-	}
-
 	Point findEmptyField() {
-		for (int i = 0; i < ROWS; i++) {
-			for (int j = 0; j < ROWS; j++) {
-				if (fields[i][j] == 0)
-					return new Point(i, j);
-			}
-		}
-		return new Point();
+		for (int i = 0; i < COLUMNS*ROWS; i++)
+			if (fields[i%ROWS][i/ROWS] == 0) return new Point(i%ROWS, i/ROWS);
+		return null;
 	}
 
-	Boolean checkValidMove(Point emptyField, int num) {
-		// Check COLUMN for similar values		
-		for (int i = 0; i < ROWS; i++) {
-			if (fields[i][emptyField.y] == num && i != emptyField.x) {
-				return false;
-			}
-		} 
-
-		 //Check ROW for similar values
-		for (int i = 0; i < ROWS; i++) {
-			if (fields[emptyField.x][i] == num && i != emptyField.y) {
-				return false;
-			}
-		}
-
-		int startX = emptyField.x - emptyField.x % 3;
-		int startY = emptyField.y - emptyField.y % 3;
-		for (int i = startX; i < startX + 3; i++) {
-			for (int j = startY; j < startY + 3; j++) {
-				if (fields[i][j] == num && !( i == emptyField.x && j == emptyField.y)) {
-					return false;
-				}
-			}
+	Boolean checkValidMove(Point p, int num) {
+		int startX = p.x - p.x % 3, startY = p.y - p.y % 3;
+		for (int i = 0; i < COLUMNS; i++) {// Check COLUMN and ROWS and 3x3 boxes for similar values
+			if ((fields[i][p.y] == num && i != p.x) || (fields[p.x][i] == num && i != p.y)) return false; // ROW/COLUMN
+			if (fields[startX+i%3][startY+i/3] == num && !( startX + i%3 == p.x && startY + i/3 == p.y)) return false;
 		}
 		return true;
-	}
-
-	Point coordToField(Point p) {
-		return new Point(p.y * COLUMNS / XSIZE, p.x * COLUMNS / XSIZE);
 	}
 
 	Point fieldToCoord(Point p) {
-		return new Point(p.y * XSIZE / COLUMNS, p.x * XSIZE / COLUMNS);
+		return new Point(p.y * SIZE / COLUMNS, p.x * SIZE / COLUMNS);
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D) g;
-
-		g2d.setColor(defaultColor);
-		g2d.fillRect(0, 0, XSIZE, YSIZE);
-		g2d.setColor(rowColor);
+		g.setColor(new Color(100, 50, 50, 50));
 		for (int line = 0; line < ROWS; line += 2) {
-			g2d.fillRect(0, line * ROW_WIDTH, XSIZE, ROW_WIDTH);
+			g.fillRect(0, line * ROW_WIDTH, SIZE, ROW_WIDTH); // Rows
+			g.fillRect(line * ROW_WIDTH, 0, ROW_WIDTH, SIZE); // Columns
 		}
-
-		g2d.setColor(columnColor);
-		for (int line = 0; line < COLUMNS; line += 2) {
-			g2d.fillRect(line * ROW_WIDTH, 0, ROW_WIDTH, YSIZE);
-		}
-
-		g2d.setColor(Color.BLACK);
-		g2d.drawString("Solve = '" + (char)SOLVE + "'", ROW_WIDTH, YSIZE + ROW_WIDTH/3 -2);
-		g2d.drawString("Clear = '" + (char)CLEAR + "'", ROW_WIDTH, YSIZE + ROW_WIDTH/3 + 10);
+		g.setColor(Color.BLACK);
+		g.drawString("Solve = '" + (char)SOLVE + "'", ROW_WIDTH, SIZE + ROW_WIDTH/3 -2);
+		g.drawString("Clear = '" + (char)CLEAR + "'", ROW_WIDTH, SIZE + ROW_WIDTH/3 + 10);
 		for (int line = 0; line < ROWS; line += 3) {
-			g2d.drawRect(0, line * ROW_WIDTH, XSIZE, ROW_WIDTH * 3);
+			g.drawRect(0, line * ROW_WIDTH, SIZE, ROW_WIDTH * 3); // border for boxes rows
+			g.drawRect(line * ROW_WIDTH, 0, ROW_WIDTH * 3, SIZE); // border for boxes columns
 		}
-
-		g2d.setColor(Color.BLACK);
-		for (int line = 0; line < ROWS; line += 3) {
-			g2d.drawRect(line * ROW_WIDTH, 0, ROW_WIDTH * 3, YSIZE);
-		}
-
-		for (int i = 0; i < ROWS; i++) {
-			for (int j = 0; j < COLUMNS; j++) {
-				if (fields[i][j] != 0) {
-					Point temp = fieldToCoord(new Point(i, j));
-					g2d.drawString(String.valueOf(fields[i][j]), temp.x
-							+ ROW_WIDTH / 2, temp.y + ROW_WIDTH / 2
-							+ HIGHT_PADDING);
+		for (int i = 0; i < ROWS*COLUMNS; i++)
+				if (fields[i%COLUMNS][i/ROWS] != 0) {
+					Point temp = fieldToCoord(new Point(i%COLUMNS, i/ROWS));
+					g.drawString(String.valueOf(fields[i%COLUMNS][i/ROWS]), temp.x+ROW_WIDTH/2, temp.y+ROW_WIDTH/2+5);
 				}
-			}
-		}
-		g2d.setColor(new Color(50, 200, 50, 50));
-		Point temp = fieldToCoord(selection);
-		g2d.fillRect(temp.x, temp.y, ROW_WIDTH, ROW_WIDTH);
+		g.setColor(new Color(50, 200, 50, 50));
+		g.fillRect(fieldToCoord(selection).x, fieldToCoord(selection).y, ROW_WIDTH, ROW_WIDTH);
 	}
 }
