b1bb54d19c4c211061233e908bda390e17c681c5
Henrik Oskarsson <hk1237@gmail.com>
2025-09-21 19:12:29 +0200

    Renamed fieldToCoord method and changed signature, now Point ftc(int, int), saving considerable space when used multiple times on a line. Renamed COLUMNS to COLS, ROW_WIDTH to ROW_W. Tightened if-statement in KeyAdapter logic. Inlining a few FOR and iF statements. Moved some initializer inside for-statements. Shaved about 10 lines

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -3,8 +3,8 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Sudoku extends JPanel {
-	final int ROWS = 9, COLUMNS = 9, SIZE = 495, ROW_WIDTH = SIZE / ROWS, SOLVE = KeyEvent.VK_S, CLEAR = KeyEvent.VK_C;
-	int[][] fields = new int[COLUMNS][ROWS];
+	final int ROWS = 9, COLS = 9, SIZE = 495, ROW_W = SIZE / ROWS, SOLVE = KeyEvent.VK_S, CLEAR = KeyEvent.VK_C;
+	int[][] fields = new int[COLS][ROWS];
 	Point selection = new Point();
 
 	public static void main(String[] args) {
@@ -15,27 +15,23 @@ public class Sudoku extends JPanel {
 	}
 
 	public Sudoku() {
-		setPreferredSize(new Dimension(SIZE + 1, SIZE + ROW_WIDTH));
+		setPreferredSize(new Dimension(SIZE + 1, SIZE + ROW_W));
 		addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
-				selection =  new Point(e.getPoint().x * COLUMNS / SIZE, e.getPoint().y * ROWS / SIZE);
+				selection =  new Point(e.getPoint().x * COLS / SIZE, e.getPoint().y * ROWS / SIZE);
 				requestFocusInWindow(); // Needed to activate key-listener
 				repaint();
 			}
 		});
-
 		addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
 				int code = e.getKeyCode();
-				if (code >= KeyEvent.VK_NUMPAD0 && code <= KeyEvent.VK_NUMPAD9) {
+				if (code >= KeyEvent.VK_NUMPAD0 && code <= KeyEvent.VK_NUMPAD9)
 					code -= KeyEvent.VK_NUMPAD0 - KeyEvent.VK_0; // Translate numpad input into normal numbers
-				}
-				if (code >= KeyEvent.VK_0 && code <= KeyEvent.VK_9) {
-					code -= KeyEvent.VK_0;
-					if(checkValidMove(selection, code)) fields[selection.x][selection.y] = code;
-				}
+				if (code >= KeyEvent.VK_0 && code <= KeyEvent.VK_9 && checkValidMove(selection, code-KeyEvent.VK_0))
+                    fields[selection.x][selection.y] = code - KeyEvent.VK_0;
 				if (code == SOLVE && findEmptyField() != null) solve();
-				if (code == CLEAR) fields = new int[COLUMNS][ROWS];
+				if (code == CLEAR) fields = new int[COLS][ROWS];
 				repaint();
 			}
 		});
@@ -52,44 +48,37 @@ public class Sudoku extends JPanel {
 	}
 
 	Point findEmptyField() {
-		for (int i = 0; i < COLUMNS*ROWS; i++)
-			if (fields[i%ROWS][i/ROWS] == 0) return new Point(i%ROWS, i/ROWS);
+		for (int i = 0; i < COLS*ROWS; i++) if (fields[i%ROWS][i/ROWS] == 0) return new Point(i%ROWS, i/ROWS);
 		return null;
 	}
 
 	Boolean checkValidMove(Point p, int num) {
-		int startX = p.x - p.x % 3, startY = p.y - p.y % 3;
-		for (int i = 0; i < COLUMNS; i++) {// Check COLUMN and ROWS and 3x3 boxes for similar values
+		for (int i = 0, x0 = p.x - p.x % 3, y0 = p.y - p.y % 3; i < COLS; i++) {
 			if ((fields[i][p.y] == num && i != p.x) || (fields[p.x][i] == num && i != p.y)) return false; // ROW/COLUMN
-			if (fields[startX+i%3][startY+i/3] == num && !( startX + i%3 == p.x && startY + i/3 == p.y)) return false;
+			if (fields[x0+i%3][y0+i/3] == num && !( x0 + i%3 == p.x && y0 + i/3 == p.y)) return false;
 		}
 		return true;
 	}
 
-	Point fieldToCoord(Point p) {
-		return new Point(p.x * SIZE / COLUMNS, p.y * SIZE / COLUMNS);
-	}
+    Point ftc(int x, int y) { return new Point(x * SIZE / COLS, y * SIZE / COLS); } // Field To Coordinate
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.setColor(new Color(100, 50, 50, 50));
 		for (int line = 0; line < ROWS; line += 2) {
-			g.fillRect(0, line * ROW_WIDTH, SIZE, ROW_WIDTH); // Rows
-			g.fillRect(line * ROW_WIDTH, 0, ROW_WIDTH, SIZE); // Columns
+			g.fillRect(0, line * ROW_W, SIZE, ROW_W); // Rows
+			g.fillRect(line * ROW_W, 0, ROW_W, SIZE); // Columns
 		}
 		g.setColor(Color.BLACK);
-		g.drawString("Solve = '" + (char)SOLVE + "'", ROW_WIDTH, SIZE + ROW_WIDTH/3 -2);
-		g.drawString("Clear = '" + (char)CLEAR + "'", ROW_WIDTH, SIZE + ROW_WIDTH/3 + 10);
+		g.drawString("Solve = '" + (char)SOLVE + "'", ROW_W, SIZE + ROW_W /3 - 2);
+		g.drawString("Clear = '" + (char)CLEAR + "'", ROW_W, SIZE + ROW_W /3 + 10);
 		for (int line = 0; line < ROWS; line += 3) {
-			g.drawRect(0, line * ROW_WIDTH, SIZE, ROW_WIDTH * 3); // border for boxes rows
-			g.drawRect(line * ROW_WIDTH, 0, ROW_WIDTH * 3, SIZE); // border for boxes columns
+			g.drawRect(0, line * ROW_W, SIZE, ROW_W * 3); // border for boxes rows
+			g.drawRect(line * ROW_W, 0, ROW_W * 3, SIZE); // border for boxes columns
 		}
-		for (int i = 0; i < ROWS*COLUMNS; i++) //
-				if (fields[i%COLUMNS][i/ROWS] != 0) {
-					Point temp = fieldToCoord(new Point(i%COLUMNS, i/ROWS));
-					g.drawString(String.valueOf(fields[i%COLUMNS][i/ROWS]), temp.x+ROW_WIDTH/2, temp.y+ROW_WIDTH/2+5);
-				}
+        for (int k = 0, i = 0, j = 0; k < ROWS* COLS; k++, i = k%COLS, j = k/COLS)
+            if (fields[i][j] != 0) g.drawString(""+fields[i][j], ftc(i, j).x+ ROW_W/2, ftc(i, j).y+ ROW_W/2+5);
 		g.setColor(new Color(50, 200, 50, 50));
-		g.fillRect(fieldToCoord(selection).x, fieldToCoord(selection).y, ROW_WIDTH, ROW_WIDTH);
+		g.fillRect(ftc(selection.x, selection.y).x, ftc(selection.x, selection.y).y, ROW_W, ROW_W);
 	}
 }
